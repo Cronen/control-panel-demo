0b5b20d736fda8357b23480e4da59fcfe2ad041e
Thomas Crone <crone1992@gmail.com>
2020-07-15 20:27:48 +0200

    merge fix

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-<<<<<<< HEAD
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
 Vue.config.productionTip = false;
@@ -11,12 +10,4 @@ Vue.use(IconsPlugin);
 new Vue({
   router,
   render: (h) => h(App),
-=======
-
-Vue.config.productionTip = false;
-
-new Vue({
-  router,
-  render: h => h(App)
->>>>>>> 6650640872bbbf445ae652b7b1e2693b5b99824c
 }).$mount("#app");
